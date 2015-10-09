49964401a01ea049ddfa97f12cdd1005ebac9f4d
Sherlock <jarrettergolocke@gmail.com>
2015-10-09 15:57:07 -0500

    Committing initial comments for documentation.

--- InClass5/GenerateSQL.c
+++ InClass5/GenerateSQL.c
@@ -1,7 +1,31 @@
+// Author: Jarrett Locke
+// Class: Database Management
+// Major: Computer Science - Game Development Consentration
+//
+// Assignment: 
+//  Given a .txt file with information. Generate an .sql file with
+//      commands that will drop the tables Student, Faculty, Class, and Enroll
+//      if they exist and then create those tables. After the tables are
+//      created, we are to populate each table with data given in the .txt file.
+//
+// Assumptions:
+//  Since the information and the file is given, I can assume it to be static.
+//      - if statements should not be required.
+//      - very straightforward code.
+//      - file is in same directory as executable.
+//
+//  Will approach this planning on working on the data in bulk.
+//      - Get all lines in file first.
+//      - Work on each line.
+//          - Grab table name or skip it?
+//          - Grab all information into variables or just directly insert
+//              data into query?
+//      - Again, probably shouldn't need an if statement.
+
 #include <stdio.h>
 
 int
 main()
 {
-    
+    // Variables go here.
 }
